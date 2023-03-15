cf5bcf3ca7658561a71a6d6f786c6cf3ad6edf7d
Daisei Tanaka <d.tnk.4004@gmail.com>
2023-03-15 16:20:09 +0900

    Initial Commit

---
+++ cLock_Timer/cLock_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cLock_TimerApp.swift
+//  cLock_Timer
+//
+//  Created by 田中大誓 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct cLock_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
